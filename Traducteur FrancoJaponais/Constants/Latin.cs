f8ae24a2251bc02e7340f12787206f4e388769c3
Hadrien Grimont <hadrien.grimont@gmail.com>
2025-12-11 20:09:59 +0100

    wip

---
+++ Traducteur FrancoJaponais/Constants/Latin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Traducteur_FrancoJaponais.Constants
+{
+    public static class Latin
+    {
+        public static List<char> _Dictionary = new List<char>() 
+        {
+            'a',
+            'b',
+            'c',
+            'd',
+            'e',
+            'f',
+            'g',
+            'h',
+            'i',
+            'j',
+            'k',
+            'l',
+            'm',
+            'n',
+            'o',
+            'p',
+            'q',
+            'r',
+            's',
+            't',
+            'u',
+            'v',
+            'w',
+            'x',
+            'y',
+            'z',
+        };
+    }
+}
