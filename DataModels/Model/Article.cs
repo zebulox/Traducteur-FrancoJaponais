d889c996d1421f0e0f38b01ab0c4ae1ce9d29fd9
Hadrien Grimont <hadrien.grimont@gmail.com>
2025-12-13 12:08:33 +0100

    added new dict

---
+++ DataModels/Model/Article.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace DataModels.Model
+{
+    public class Forme
+    {
+        public string Vedette { get; set; }
+        public string Vedetteféminin { get; set; }
+        public string Gram { get; set; }
+    }
+
+    public class Étiquettes
+    {
+        public object Domaine { get; set; }
+        public object Registre { get; set; }
+    }
+
+    public class Segment
+    {
+        public String Jpn { get; set; }
+        public string Fra { get; set; }
+    }
+
+    public class Segments
+    {
+        public List<Segment> Segment { get; set; }
+    }
+
+    public class Sens
+    {
+        public Étiquettes Étiquettes { get; set; }
+        public Segments Segments { get; set; }
+        public String N { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class Sémantique
+    {
+        public List<Sens> Sens { get; set; }
+    }
+
+    public class Article
+    {
+        public Forme Forme { get; set; }
+        public Sémantique Sémantique { get; set; }
+        public string Id { get; set; }
+        public string Text { get; set; }
+    }
+}
