55c1e7794236294b1f124432042e108fe0e62d38
Hadrien Grimont <hadrien.grimont@gmail.com>
2025-12-21 19:40:37 +0100

    start changes to dico page with new model

--- Traducteur FrancoJaponais/MauiProgram.cs
+++ Traducteur FrancoJaponais/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using DataModels.Model;
+using DataModels.Model.Dictionary;
 using Microsoft.Extensions.Logging;
 using Plugin.Maui.OCR;
 using Traducteur_FrancoJaponais.Services.DataBase;
@@ -24,6 +25,14 @@ namespace Traducteur_FrancoJaponais
             builder.Services.AddSingleton<IDataBaseService>(new DataBaseService());
             builder.Services.AddSingleton<IDataManager<HiromiCourse>>(new DataManagerService<HiromiCourse>(builder.Services.BuildServiceProvider().GetService<IDataBaseService>()));
             builder.Services.AddSingleton<IDataManager<HiromiPhrase>>(new DataManagerService<HiromiPhrase>(builder.Services.BuildServiceProvider().GetService<IDataBaseService>()));
+            builder.Services.AddSingleton<IDataManager<Word>>(new DataManagerService<Word>(builder.Services.BuildServiceProvider().GetService<IDataBaseService>()));
+            builder.Services.AddSingleton<IDataManager<FrenchWord>>(new DataManagerService<FrenchWord>(builder.Services.BuildServiceProvider().GetService<IDataBaseService>()));
+            builder.Services.AddSingleton<IDataManager<JapaneseWord>>(new DataManagerService<JapaneseWord>(builder.Services.BuildServiceProvider().GetService<IDataBaseService>()));
+            builder.Services.AddSingleton<IDataManager<Tag>>(new DataManagerService<Tag>(builder.Services.BuildServiceProvider().GetService<IDataBaseService>()));
+            builder.Services.AddSingleton<IDataManager<WordFrench>>(new DataManagerService<WordFrench>(builder.Services.BuildServiceProvider().GetService<IDataBaseService>()));
+            builder.Services.AddSingleton<IDataManager<WordJapanese>>(new DataManagerService<WordJapanese>(builder.Services.BuildServiceProvider().GetService<IDataBaseService>()));
+            builder.Services.AddSingleton<IDataManager<WordTag>>(new DataManagerService<WordTag>(builder.Services.BuildServiceProvider().GetService<IDataBaseService>()));
+
             builder.Services.AddSingleton<IPermissionManager>(new PermissionManager());
             builder.Services.AddSingleton<IDataBaseInitializer>(new DataBaseInitializer(builder.Services.BuildServiceProvider().GetService<IDataBaseService>().Getdatabase()));
             /*Faire la DI ici ex:*/
