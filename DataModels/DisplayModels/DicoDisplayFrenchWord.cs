f8577140078db23a0013436b9a0282ed15d933e6
Hadrien Grimont <hadrien.grimont@gmail.com>
2025-12-22 19:38:45 +0100

    recherche dans le dico en place encore du taf

---
+++ DataModels/DisplayModels/DicoDisplayFrenchWord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModels.DisplayModels
+{
+    public class DicoDisplayFrenchWord
+    {
+        public int Id { get; set; }
+        public String ResearchString { get; set; }
+        public String French { get; set; }
+        public List<String> Japanese { get; set; }
+        public List<String> Tags { get; set; }
+    }
+}
