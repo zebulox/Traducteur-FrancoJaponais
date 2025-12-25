e5bfef2090be4bce46c75d5435d9079490101e47
Hadrien Grimont <hadrien.grimont@gmail.com>
2025-12-25 19:12:19 +0100

    start coloring badges

---
+++ Traducteur FrancoJaponais/Constants/TagCss.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Traducteur_FrancoJaponais.Constants
+{
+    public static class TagCss
+    {
+        public static List<String> Orthographe = new List<string>()
+        {
+            "&#x27;ku&#x27; adjective (archaic)",
+            "&#x27;shiku&#x27; adjective (archaic)",
+            "adjective (generic)",
+            "adverb",
+            "adverb taking the と particle",
+            "archaic/formal form of na-adjective",
+            "auxiliary",
+            "auxiliary adjective",
+            "auxiliary verb",
+            "conjunction",
+            "copula",
+            "godan verb",
+            "godan verb (archaic)",
+            "grammar",
+            "Ichidan verb",
+            "interjection",
+            "intransitive verb",
+            "irregular nu verb",
+            "irregular okurigana",
+            "irregular ru verb",
+            "kuru verb",
+            "male term",
+            "nidan verb (archaic)",
+            "noun",
+            "noun (generic)",
+            "noun (prefix)",
+            "noun (suffix)",
+            "noun or participle taking the aux. verb する",
+            "noun or verb acting prenominally",
+            "noun taking the genitive case particle の",
+            "particle",
+            "prefix",
+            "pre-noun adjectival",
+            "pronoun",
+            "su verb - precursor to the modern suru",
+            "suffix",
+            "suru verb",
+            "transitive verb",
+            "unclassified",
+            "unclassified name",
+            "verb (generic)",
+            "yodan verb (archaic)",
+            "い adjective",
+            "たる adjective",
+            "な adjective",
+        };
+    }
+}
